559779268ee3a801bd022834a817fed4f7bd5586
Rohit Roy <rohitdevops.roy@gmail.com>
2021-07-26 14:35:50 +0530

    14.35

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+
+var principal = document.getElementById("principal").value;
+
+var rate = document.getElementById("rate").value;
+
+var years = document.getElementById("years").value;
+
+var interest = principal * years * rate /100;
+
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
 function compute()
 {
     p = document.getElementById("principal").value;
     
 }
-        
