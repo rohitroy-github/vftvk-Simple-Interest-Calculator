68b2728db3b00b963645a206c21a9b84803f4eb0
Rohit Roy <rohitdevops.roy@gmail.com>
2021-07-26 20:38:23 +0530

    2038

--- script.js
+++ script.js
@@ -18,17 +18,18 @@ function compute()
     var yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
     
     var resultDisplay = document.getElementById("result");
+    //final output print statement 
     resultDisplay.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>."  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
 }
 
 function SliderValue()
 {
-    var slider = document.getElementById("rate");
+    var rateval = document.getElementById("rate");
     var output = document.getElementById("rate_val");
-    output.innerHTML = slider.value; // Display the default slider value
+    output.innerHTML = rateval.value; // Display the default slider value
 
     // Update the current slider value (each time you drag the slider handle)
-    slider.oninput = function() 
+    rateval.oninput = function() 
     {
         output.innerHTML = this.value;
     }  
